3af5a086d3410f0905389f18b5667892fd7b807c
saber2pr <saber2pr@gmail.com>
2019-02-04 19:08:16 +0800

    update

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,8 +1,6 @@
 import { test_canvas } from './test_canvas'
-import { test_vector } from './test_vector'
 
 test_canvas()
-// test_vector()
 // const ctx = document.getElementsByTagName('canvas')[0].getContext('2d')
 // let img = new Image()
 // img.src = 'https://ubmcmm.baidustatic.com/media/v1/0f000F_LSh3bh3jmLIlBv6.png'
